8eb673b465acd9a7d2a3c761c666227022131cde
tingkao <tingkao4124@gmail.com>
2019-05-01 00:38:25 +0800

    week2 hw5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,23 @@
 function join(str, concatStr) {
-  return concatStr;
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (i === 0) {
+      result += str[i];
+    } else {
+      result += concatStr + str[i];
+    }
+  }
+  return result;
 }
+
 
 function repeat(str, times) {
-  return times;
+  let result = '';
+  for (let i = 0; i < times; i += 1) {
+    result += str;
+  }
+  return result;
 }
 
-console.log(join('a', '!'));
+console.log(join(['a', 'b', 'c'], '!'));
 console.log(repeat('a', 5));
