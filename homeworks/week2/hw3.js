745ad6cd2fd6a33476589c043457d66b5ee67123
tingkao <tingkao4124@gmail.com>
2019-05-01 00:23:14 +0800

    week2 hw3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  return str;
+  const array = str.split('');
+  let result = '';
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    result += array[i];
+  }
+  return result;
 }
 
-reverse('hello');
+console.log(reverse('hello'));
