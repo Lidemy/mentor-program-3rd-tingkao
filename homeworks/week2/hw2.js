58a560b1197161e93ad9fdfa51dca440c9004d65
tingkao <tingkao4124@gmail.com>
2019-04-30 22:36:42 +0800

    week2 hw2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,10 @@
 function capitalize(str) {
-  return str;
+  const first = str[0].toUpperCase();
+  let others = '';
+  for (let i = 0; i < str.length - 1; i += 1) {
+    others += str[i + 1];
+  }
+  return first + others;
 }
 
 console.log(capitalize('hello'));
