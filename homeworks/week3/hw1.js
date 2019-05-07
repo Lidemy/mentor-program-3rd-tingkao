e1a729f5e52b1ae05f4d52af4621640f0c4acf2e
tingkao <tingkao4124@gmail.com>
2019-05-07 22:02:43 +0800

    hw1

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,14 @@
 function stars(n) {
-  console.log(n);
+  const array = [];
+  for (let i = 1; i <= n; i += 1) {
+    const num = i;
+    let total = '';
+    for (let a = 0; a < num; a += 1) {
+      total += '*';
+    }
+    array.push(total);
+  }
+  return array;
 }
 
 module.exports = stars;
