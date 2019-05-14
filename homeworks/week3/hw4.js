50f4f4b7af9ccffeee6c1304ba7243492fe51742
tingkao <tingkao4124@gmail.com>
2019-05-15 01:33:19 +0800

    hw4

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,12 @@
 function isPalindromes(str) {
-  console.log(str);
+  const reStr = str;
+  let answer = true;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] !== reStr[str.length - 1 - i]) {
+      answer = false;
+    }
+  }
+  return answer;
 }
 
 module.exports = isPalindromes;
