f7d8ad7196e003538e9e869d6a21a67aeddb4bdc
tingkao <tingkao4124@gmail.com>
2019-05-15 01:30:14 +0800

    hw3

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,15 @@
 function isPrime(n) {
-  console.log(n);
+  let answer = true;
+  if (n <= 1) {
+    answer = false;
+  } else if (n > 2) {
+    for (let i = 2; i < n; i += 1) {
+      if ((n % i) === 0) {
+        answer = false;
+      }
+    }
+  }
+  return answer;
 }
 
 module.exports = isPrime;
